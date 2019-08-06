c9d6aab742fb472b7d1e7fcb586c042ba1f041a6
Jenian <38227005+jeniantai@users.noreply.github.com>
2019-08-06 09:08:33 -0400

    Update dataTypes.py

--- Day 1/dataTypes.py
+++ Day 1/dataTypes.py
@@ -1,13 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
 # Declare second integer, double, and String variables.
-i2 = int(raw_input())
-d2 = float(raw_input())
-s2 = raw_input()
+n = int(input())
+f = float(input())
+w = str(input())
 # Read and save an integer, double, and String to your variables.
 
 # Print the sum of both integer variables on a new line.
-print (i + i2)
+print (i + n)
 # Print the sum of the double variables on a new line.
-print (d + d2)
+print (d + f)
 # Concatenate and print the String variables on a new line
 # The 's' variable above should be printed first.
-print (s+s2)
+print (s+w)
